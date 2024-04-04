a51d3782ba145af2a911b8a92c1f17ddd05082b2
71M073J <ciglarictimotej@gmail.com>
2024-04-04 11:27:57 +0200

    fix batch size for cifar

--- resnet18-long_test.py
+++ resnet18-long_test.py
@@ -47,11 +47,11 @@ if __name__ == "__main__":
         tf_test = transforms.Compose(tf_test)
         dataset1 = torch.utils.data.DataLoader(
             torchvision.datasets.CIFAR10(
-                root='./data', train=True, download=True, transform=tf), batch_size=128, shuffle=True, num_workers=4)
+                root='./data', train=True, download=True, transform=tf), batch_size=bs, shuffle=True, num_workers=4)
 
         dataset2 = torch.utils.data.DataLoader(
             torchvision.datasets.CIFAR10(
-                root='./data', train=False, download=True, transform=tf_test), batch_size=100, shuffle=False,
+                root='./data', train=False, download=True, transform=tf_test), batch_size=bs, shuffle=False,
             num_workers=4)
 
     net = torchvision.models.resnet18()
